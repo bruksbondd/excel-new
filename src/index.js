dcffd6162b076ead47b581d883163ec52b64a479
Serhi Brekhov <bruksbond@gmail.com>
2020-10-01 16:44:06 +0300

    Finish base framework function (#4)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import {Excel} from '@/components/excel/Excel'
 import './scss/index.scss'
+import {Header} from '@/components/header/Header'
+import {Tollbar} from '@/components/toolbar/Tollbar'
+import {Formula} from '@/components/formula/Formula'
+import {Table} from '@/components/table/Table'
 
-console.log('Working!')
+const excel = new Excel('#app', {
+  components: [Header, Tollbar, Formula, Table]
+})
+
+console.log('Excel', excel.render())
